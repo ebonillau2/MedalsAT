f16d94fdb0c080095c1fb391a8342861db0f4f86
Enrique Bonilla <ebonillau@proveedor.intercorp.com.pe>
2025-10-22 19:08:02 -0500

    Initial Commit

---
+++ MedalsATUITests/MedalsATUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  MedalsATUITestsLaunchTests.swift
+//  MedalsATUITests
+//
+//  Created by Enrique Bonilla on 22/10/25.
+//
+
+import XCTest
+
+final class MedalsATUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
